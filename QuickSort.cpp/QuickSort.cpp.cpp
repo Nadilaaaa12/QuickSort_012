067359990e0149e0a84103bc0cb51fd0e7e89703
Nadilaaaa12 <nadilazaskiaauliaaulia@gmail.com>
2023-03-30 10:55:49 +0700

    Menambahkan Fungsi Input

--- QuickSort.cpp/QuickSort.cpp.cpp
+++ QuickSort.cpp/QuickSort.cpp.cpp
@@ -7,7 +7,6 @@ int cmp_count = 0; // number of comparasion
 int mov_count = 0; // number of data movements
 int n;
 
-
 void input() {
 	while (true)
 	{
@@ -17,7 +16,17 @@ void input() {
 		if (n <= 20)
 			break;
 		else
-			cout << "\nMaksimum panjang array adalah 20" << endl;
-		
+			cout << "\nMaksimum panjang array adalah 20" << endl;	
+	}
+
+	cout << "\n====================" << endl;
+	cout << "\nEnter Array Element" << endl;
+	cout << "\n==================" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
 	}
 }
+//swaps the element at index x with the element at index y
